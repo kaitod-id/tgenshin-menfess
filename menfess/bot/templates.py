02c8d8436f812576e1041600cc238451d1a93d5a
kaitod-id <108166655+kaitod-id@users.noreply.github.com>
2023-08-17 00:00:18 +0700

    Update templates.py

--- menfess/bot/templates.py
+++ menfess/bot/templates.py
@@ -9,7 +9,55 @@ def get_traveler_name(user):
     if user.username:
         return f"@{user.username}"
     elif user.first_name:
-        return user.first_name
+        return user.import os
+
+
+def get_user_name(user):
+    if user.first_name:
+        if user.last_name:
+            return f"[{user.first_name} {user.last_name}](tg://user?id={{user.id}})"
+        return f"[{user.first_name}](tg://user?id={{user.id}})"
+    return f"[{user.username}](tg://user?id={{user.id}})" if user.username else "Pengguna"
+
+user_id = "<nama_pengguna>"  # Ganti dengan nilai yang sesuai
+__TRAVELER = f"{get_user_name(user)}"
+__TEYVAT = f"[Teyvat](t.me/{os.getenv('CHANNEL_USERNAME')})"
+__RULES = """- Akun Telegram {_TRV} harus follow {_TVT} dulu ya, ehe~.
+- Akun Telegram {_TRV} harus memiliki username dan foto profil.
+- Pesan {_TRV} harus berjumlah minimal 20 karakter dan 5 kata.""".format(_TRV=__TRAVELER, _TVT=__TEYVAT)
+
+
+ON_START_MSG = """
+Halo {_TRV}!, selamat datang di {bot_name}.
+Aku adalah bot untuk membawa pesan kamu ke alam {_TVT},
+{_TRV} kalau mau tau cara kirim pesan ke {_TVT} tinggal tekan tombol di bawah ya, ehe!
+""".format(_TRV=__TRAVELER, _TVT=__TEYVAT, bot_name="{bot_name}")
+
+
+ON_HOWTO_CB = """
+{_TRV} tinggal ketik apapun yang {_TRV} mau yang berhubungan dengan Genshin Impact ya {_TRV}, tetapi
+pesan {_TRV} akan terkirim jika pesan {_TRV} memenuhi syarat di bawah ini:
+
+{_RULE}
+
+Udah itu aja {_TRV}! enjoy flexing gacha dan damage karakter {_TRV} ya, ehe!
+""".format(_TRV=__TRAVELER, _TVT=__TEYVAT, _RULE=__RULES)
+
+
+ON_SUCCESS_POST = """
+Hore, pesan {_TRV} telah terkirim ke {_TVT}!
+{_TRV} bisa melihatnya dengan menekan tombol di bawah ya~.
+""".format(_TRV=__TRAVELER, _TVT=__TEYVAT)
+
+
+ON_FAILED_POST = """
+Yah, pesan {_TRV} belum memenuhi kriteria di bawah ini:
+
+{_RULE}
+
+Jadi Kamu gabisa mengirim pesan {_TRV}, hiks.
+""".format(_TRV=__TRAVELER, _TVT=__TEYVAT, _RULE=__RULES)
+
     else:
         return "Traveler"
 
